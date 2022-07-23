4c864843d3998b56f892059b0effe5a2a25018fb
Victor Drozd <victor.drozd@gmail.com>
2022-07-23 22:51:31 +0300

    refactored pages and removed old test cases

---
+++ ApplicationPages/GlobalSQA/SamplePageTestPage.cs
@@ -0,0 +1,22 @@
+﻿using OpenQA.Selenium;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ApplicationPages.GlobalSQA
+{
+    public class SamplePageTestPage : BasePage
+    {
+        public SamplePageTestPage(IWebDriver driver) : base(driver) { }
+
+        private IWebElement _profilePicInput => _driver.FindElement(By.CssSelector("[name='file-553']"));
+        private IWebElement _experienceDropdown => _driver.FindElement(By.CssSelector("#g2599-experienceinyears"));
+        private List<IWebElement> _checkboxes => _driver.FindElements(By.CssSelector("input.checkbox-multiple")).ToList();
+        private List<IWebElement> _radioButtons => _driver.FindElements(By.CssSelector("input.radio")).ToList();
+        private IWebElement commentTextArea => _driver.FindElement(By.CssSelector("#contact-form-comment-g2599-comment"));
+        private IWebElement submitButton => _driver.FindElement(By.CssSelector("button.pushbutton-wide"));
+
+
+
+
+    }
+}
---
+++ Assignment_18_VD/BaseTest.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace Assignment_18_VD
+{
+    public class BaseTest
+    {
+        protected IWebDriver driver;
+
+        [SetUp]
+        public void BeforeTest()
+        {
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.AddArgument("--window-size=1400,1000");
+            driver = new ChromeDriver(chromeOptions);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(15);
+
+        }
+
+        [TearDown]
+        public void AfterTests()
+        {
+            driver.Quit();
+        }
+    }
+}
